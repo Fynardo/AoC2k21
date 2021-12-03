26489d2909ff75e9bf8b1ae8b6230fc369e0c39c
Diego <dfynar@gmail.com>
2021-12-04 00:05:08 +0100

    day 3 part 2

--- day3/diagnostic.py
+++ day3/diagnostic.py
@@ -9,7 +9,6 @@ def read_report(case):
 
 
 # Part 1
-
 def check_consumption(report):
     n = len(report) # rows
     m = len(report[0]) # cols
@@ -36,3 +35,60 @@ report = read_report('input')
 chk = check_consumption(report)
 print('Done!')
 print(f'Consumption: {chk}')
+
+
+# Part 2
+def count_ones(report, idx):
+    count = 0
+    for word in report:
+        count += int(word[idx])
+    return count
+
+## oxygen
+def check_oxygen(report, i):
+    # Recursive because why not
+    if len(report) == 1:
+        return report[0]
+    
+    count = count_ones(report, i)
+    n = len(report)
+
+    b = '1' if count >= n/2 else '0'
+    report = list(filter(lambda x: x[i] == b, report))
+    # print(i, b, count, n)
+    return check_oxygen(report, i+1)
+
+## co2
+def check_co2(report, i):
+    # The same as above because no time :_
+    if len(report) == 1:
+        return report[0]
+    
+    count = count_ones(report, i)
+    n = len(report)
+
+    b = '0' if count >= n/2 else '1'
+    report = list(filter(lambda x: x[i] == b, report))
+    # print(i, b, count, n)
+    return check_co2(report, i+1)
+
+
+print('Part 2. Testing... ', end=' ')
+report = read_report('test')
+oxygen = check_oxygen(report, 0)
+report = read_report('test')
+co2 = check_co2(report, 0)
+assert int(oxygen, 2) * int(co2, 2) == 230
+print('Done!')
+
+
+print('Part 2...', end=' ')
+report = read_report('input')
+oxygen = check_oxygen(report, 0)
+report = read_report('input')
+co2 = check_co2(report, 0)
+chk = int(oxygen, 2) * int(co2, 2)
+print('Done!')
+print(f'Life Support: {chk}')
+
+
