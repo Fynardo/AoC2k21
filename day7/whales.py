b01278224c29eb37b67d3637bde2b3b67550d2d5
Diego <dfynar@gmail.com>
2021-12-07 12:18:12 +0100

    Day 7 part 2

--- day7/whales.py
+++ day7/whales.py
@@ -8,14 +8,11 @@ def scan_crabs(case):
     return crabs
 
 
+# Part 1
 def calculate_fuel(i, crabs):
-    """ Calculates fuel consumption of every crab moving to 'i' position """
+    """ Calculates fuel consumption of every crab moving to 'i' position. Constant consumption """
     return sum([abs(crab - i) for crab in crabs])
 
-
-
-
-# Part 1
 def arrange_crabs(case):
     """ I suppose that the cheapest move is to go to the middle of the pack, the median. """
     crabs = scan_crabs(case)
@@ -34,5 +31,45 @@ print('Done!')
 print(f'Total fuel consumption: {arrange_crabs("input")}')
 
 
+# Part 2
+def calculate_fuel(i, crabs):
+    """ Calculates fuel consumption of every crab moving to 'i' position. Variable consumption """
+    f = lambda x: sum(range(x+1))
+    return sum([f(abs(crab - i)) for crab in crabs])
+
+
+def arrange_crabs(case):
+    """
+        Median is not the cheapest position anymore, but it is a good starting point.
+        Just keep looking in the direction that minimizes the objective.
+        Which I suppose that will be to the right (positions are limited at 0, so the values that will 
+        generate tons of fuel consumption are goint to be the big ones)
+        There should be just one minimum, the optimal one.
+    """
+    crabs = scan_crabs(case)
+    m = sorted(crabs)
+    n = len(crabs)
+    start = m[n // 2]
+    fuel = calculate_fuel(start, crabs)
+    
+    next_ = start + 1
+    next_fuel = calculate_fuel(next_, crabs)
+
+    while (next_fuel < fuel):
+        next_ += 1
+        fuel = next_fuel
+        next_fuel = calculate_fuel(next_, crabs)
+
+    return fuel
+    
+
+print('Part 2. Testing...', end=' ')
+assert arrange_crabs('test') == 168
+print('Done!')
+
+print('Part 2...', end=' ')
+fuel = arrange_crabs('input')
+print('Done!')
+print(f'Total fuel consumption: {fuel}')
 
 
