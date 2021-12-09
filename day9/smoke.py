6f34ac1471e0ed73551a14d7da72b62ad35a9dd2
Diego <dfynar@gmail.com>
2021-12-09 21:45:20 +0100

    day 9, part 2. Hate neg indices :_

--- day9/smoke.py
+++ day9/smoke.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
+import functools
+
+
 def read_basin(case):
     with open(f'day9/{case}', 'r') as f:
         floor = [[int(x) for x in line] for line in f.read().splitlines()]
@@ -9,11 +12,19 @@ def read_basin(case):
 
 
 directions = ['N', 'E', 'S', 'W']
+d_builder = {'N': [-1,0], 'E':[0,1], 'S':[1,0], 'W':[0,-1]}
+
+def plan_route(i, j, d):
+    x, y = d_builder[d]
+    if i+x < 0 or j+y < 0:
+        raise IndexError
+    else:
+        return i+x, j+y
+    
 
 def peek(floor, i, j, d):
-    d_builder = {'N': [-1,0], 'E':[0,1], 'S':[1,0], 'W':[0,-1]}
-    x,y = d_builder[d]
-    return floor[i+x][j+y]
+    x,y = plan_route(i,j,d)
+    return floor[x][y]
 
 
 def is_lower(floor, i, j):
@@ -28,7 +39,6 @@ def is_lower(floor, i, j):
 
 
 # Part 1
-
 def find_lowers(case):
     risk_level = 0
     lower_points = []
@@ -50,4 +60,54 @@ print('Part 1.', end=' ')
 lowers, risk_level = find_lowers('input')
 print('Done!')
 print(f'Evaluated Risk Level: {risk_level}')
+
+
+# Part 2
+def flows_upwards(floor, i, j, d):
+    try:
+        p = peek(floor, i, j, d)
+        if p < 9 and p > floor[i][j]:
+            return True
+        else:
+            return False
+    except IndexError:
+        return False
+
+
+def locate_basins(case):
+    floor = read_basin(case)
+    basins = []
+    lowers, _ = find_lowers(case)
+
+    for l in lowers:
+        explored = set()
+        basin_elements = set()
+        frontier = [l]
+
+        while frontier:
+            i, j = frontier.pop(0)
+            if (i,j) not in explored:
+                explored.add((i,j))
+                for d in directions:
+                    if flows_upwards(floor, i, j, d):
+                        if (i, j) not in basin_elements:
+                            basin_elements.add((i, j))
+                        frontier.insert(0, plan_route(i, j, d))
+                        if plan_route(i, j, d) not in basin_elements:
+                            basin_elements.add(plan_route(i, j, d))
+        basins.append(len(basin_elements))
+    return basins
+
+
+print('Part 2. Testing...', end=' ')
+basins = locate_basins('test')
+assert functools.reduce(lambda x,y: x * y, sorted(basins, reverse=True)[:3]) == 1134
+print('Done!')
+
+
+print('Part 2.', end=' ')
+basins = locate_basins('input')
+total = functools.reduce(lambda x,y: x * y, sorted(basins, reverse=True)[:3])
+print('Done!')
+print(f'Located basins, multiplied size: {total}')
 
